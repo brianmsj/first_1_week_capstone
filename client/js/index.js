aa26916e1d13ec5bc504592e383a841be1f16c3e
brianmsj <brianmsj@gmail.com>
2017-03-17 09:04:21 -0700

    added server file and cheeseList component

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,19 @@
 import 'babel-polyfill';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+import React from 'react';
+import CheeseList from './components/cheese-list';
+import ReactDOM from 'react-dom';
+
+const newCheese = [
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue"
+];
+document.addEventListener('DOMContentLoaded', () =>
+  ReactDOM.render(
+    <CheeseList cheeses={newCheese} />,
+    document.getElementById('app')
+  )
+);
--- server/index.js
+++ server/index.js
@@ -1,14 +1,42 @@
 import 'babel-polyfill';
 import express from 'express';
-
+const bodyParser = require('body-parser');
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
+const router = express.Router();
+const jsonParser = bodyParser.json();
 
 console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
 const app = express();
 
 app.use(express.static(process.env.CLIENT_PATH));
+
+const cheeses = [
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue",
+    "Cheshire Blue",
+    "Devon Blue",
+    "Dorset Blue Vinney",
+    "Dovedale",
+    "Exmoor Blue",
+    "Harbourne Blue",
+    "Lanark Blue",
+    "Lymeswold",
+    "Oxford Blue",
+    "Shropshire Blue",
+    "Stichelton",
+    "Stilton",
+    "Blue Wensleydale",
+    "Yorkshire Blue"
+];
+
+app.get('/cheeses', (req, res) => {
+  res.json({
+    cheeses
+  });
+});
 
 function runServer() {
     return new Promise((resolve, reject) => {
